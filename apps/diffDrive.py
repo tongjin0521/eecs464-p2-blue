023382f7cec41fd4c90e5444ffc546c11f38a729
root <root@modlab.(none)>
1999-12-31 19:12:57 -0500

    Made some edits to diffDrive ... need to change the way that motor angular rates are read, also need to use sensor readings to improve orientation estimates -U

--- apps/diffDrive.py
+++ apps/diffDrive.py
@@ -2,7 +2,7 @@ from joy import *
 from joy.remote import Sink as RemoteSink
 import ckbot.dynamixel as dynamixel
 import ckbot.logical as logical
-from math import copysign, cos, sin, pi, atan2
+from math import copysign, cos, sin, pi, atan2, sqrt
 from time import time as now, sleep
 from numpy import matrix 
 
@@ -49,7 +49,7 @@ class diffDriveApp( JoyApp ):
         self.drive = 0
         self.turn = 0
         self.k_drive = 1
-        self.k_turn = 0.25 
+        self.k_turn = 0.2 
         self.i_turn = 0
 
         self.LW_sign = 1
@@ -57,12 +57,15 @@ class diffDriveApp( JoyApp ):
         self.Turret_sign = -1
 
         # Lowpass filters for the wheel rpms 
-        self.l_filter = lowpass( tau=0.2 )
-        self.r_filter = lowpass( tau=0.2 )
+        self.l_filter = lowpass( tau=1.0 )
+        self.r_filter = lowpass( tau=1.0 )
         
         # Diff drive state 
         self.pose = matrix([0.0, 0.0, 0.0])
         self.state_t0 = now()
+        
+        self.cmd_l = lowpass( tau=1.0 )
+        self.cmd_r = lowpass( tau=1.0 )
 
         # Turret state_t0
         self.turret_pose = 0.0
@@ -73,6 +76,10 @@ class diffDriveApp( JoyApp ):
         # Waypoints 
         self.waypoints = None
         self.init_pos = False
+        self.wp_cnt = 1
+
+        # Light sensors, eventually will use these to update orientation estimates
+        self.line_sense = []
         return        
     
     def stop( self ):
@@ -83,10 +90,6 @@ class diffDriveApp( JoyApp ):
 
     def update_control( self, phase ):    
 
-        # Update control inputs for wheels
-        self.r.LW.set_torque( self.LW_sign * (self.k_drive*self.drive - self.k_turn*self.turn) )
-        self.r.RW.set_torque( self.RW_sign * (self.k_drive*self.drive + self.k_turn*self.turn) )
-
         # I like being able to get voltage ...
         voltage = self.r.LW.get_voltage()
         progress( "Voltage: %5.2f" % voltage )
@@ -96,9 +99,19 @@ class diffDriveApp( JoyApp ):
             self.stop()
             return
 
+        # Update control inputs for wheels
+        self.r.LW.set_torque( self.LW_sign * (self.k_drive*self.drive - self.k_turn*self.turn) )
+        self.r.RW.set_torque( self.RW_sign * (self.k_drive*self.drive + self.k_turn*self.turn) )
+
+        """
         # If we have yet to set our current position, set it to the location
         # of the first waypoint
         if not self.waypoints:
+            return
+
+        if self.wp_cnt+1 > len(self.waypoints):
+            progress( "Finished" )
+            self.stop()
             return
         
         if not self.init_pos:
@@ -107,29 +120,42 @@ class diffDriveApp( JoyApp ):
             self.init_pos = True
             
         cur_pos = self.pose[0,:2] 
-        dir_vec = self.waypoints[1] - cur_pos
-        progress("dir_vec: %s" % repr( dir_vec ))
-        theta_des = atan2( dir_vec[0,0], dir_vec[0,1] )
+        goal_pos = self.waypoints[self.wp_cnt] 
+        dir_vec = goal_pos - cur_pos
+        ##progress("dir_vec: %s" % repr( dir_vec ))
+        theta_des = -atan2( dir_vec[0,0], dir_vec[0,1] )
         progress( "theta_des: %f" % theta_des )
 
         # Turn towards waypoint 
         theta_cur = self.pose[0,2]        
         error = theta_des - theta_cur  
-        progress("error: %f" % error)     
-        self.i_turn += 0.05*error
-        w = self.k_turn * error + self.i_turn 
+        ##progress("error: %f" % error)     
+        #self.i_turn += 0.001*error
+        w = self.k_turn * error + self.i_turn
+        if abs(w) > 0.4:
+            w = copysign(0.4, w)
         w_l = -w/2.0
         w_r = w/2.0
 
         # Calculate remaining command authority 
-        max_cmd = 0.2
+        max_cmd = 0.3
         cmd_remaining = max_cmd - (abs(w_l)+abs(w_r))        
         if cmd_remaining < 0.0:
             cmd_remaining = 0.0
-        """
-        self.r.LW.set_torque( self.LW_sign * ( w_l + cmd_remaining ) )
-        self.r.RW.set_torque( self.RW_sign * ( w_r +cmd_remaining ) )
-        """
+
+        # Calculate distance from desired location
+        dist = sqrt( (goal_pos[0,0] - cur_pos[0,0])**2 + (goal_pos[0,1] - cur_pos[0,1])**2 )    
+        progress( "Distance from wp%i: %f" % (self.wp_cnt, dist) )
+        
+        # stop if we are close to the goal
+        if dist < 0.05:
+            self.wp_cnt += 1
+            return
+        
+        lcmd = self.cmd_l.update( self.LW_sign * ( w_l + cmd_remaining/2.0 ) )
+        rcmd = self.cmd_r.update( self.RW_sign * ( w_r +cmd_remaining/2.0 ) )
+        self.r.LW.set_torque( lcmd )
+        self.r.RW.set_torque( rcmd )
         
 
     def update_turret( self, phase ):
@@ -161,6 +187,7 @@ class diffDriveApp( JoyApp ):
         if abs(cmd) > self.turret_max:
             cmd = copysign( self.turret_max, cmd )
         self.r.Turret.set_torque( cmd )
+        """
 
     def update_pose( self , phase ):
         """
@@ -185,8 +212,8 @@ class diffDriveApp( JoyApp ):
         
         # Convert wheel rpm values into velocity and angular rate
         ### NOTE: I need to get all the conversions correct here
-        wheel_radius = 0.0625
-        base_width = 0.195
+        wheel_radius = 0.062
+        base_width = 0.165
         v = ((r_rads + l_rads)*wheel_radius)/2.0
         w = ((r_rads - l_rads)*wheel_radius)/base_width
         qdot = matrix([v, w])
@@ -259,14 +286,18 @@ class diffDriveApp( JoyApp ):
         if self.timeToGet():            
             for msg in self.fieldInput.queueIter():
                 msg = msg[1]
-                if not msg.has_key('w'):
-                    continue                
-                self.waypoints = []
-                for wp in msg['w']:
-                    self.waypoints.append(matrix([-wp[0], wp[1]])/100.0)
+                if msg.has_key('w'):
+                    self.waypoints = []
+                    scale_factor = 57.0/100.0
+                    for wp in msg['w']:
+                        self.waypoints.append(scale_factor*matrix([-wp[0], wp[1]])/100.0)
+                elif msg.has_key('b') and msg.has_key('f'):
+                    self.line_sense = []
+                    self.line_sense = [msg['b'], msg['f']]
                     
-            progress('Waypoints %s'
-                     % repr(self.waypoints) )
+                    
+            #progress('Waypoints %s'
+            #         % repr(self.waypoints) )
 
         # Handle keyboard events 
         if evt.type == KEYDOWN:
