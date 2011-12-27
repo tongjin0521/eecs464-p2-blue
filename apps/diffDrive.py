4634be93167984d39233fdd994606e67faf6e214
Uriah <uriahjb@gmail.com>
2011-12-27 15:16:06 -0500

    Fixed bug in dynamixel-config.py, started on diffDrive app -U
    Former-commit-id: 7422d054628a99969b74e05066685543834d2873

---
+++ apps/diffDrive.py
@@ -0,0 +1,34 @@
+from joy import *
+import ckbot.dynamixel as dynamixel
+import ckbot.logical as logical
+from math import copysign, cos, sin, pi
+
+class diffDriveApp( JoyApp ):
+    """
+    Diff drive using 2 dynamixel servos controlled via joystick
+    """
+    def __init__( self, *args, **kw ):
+        """
+        Initialize the protocol, and app
+        """
+        p = dynamixel.Protocol()
+        kw.update( robot=dict(protocol=p, count=3) )
+        JoyApp.__init__( self, confPath="$/cfg/diffDriveApp.yml", *args, **kw )
+        self.r = self.robot.at
+        return
+        
+    """
+    def stop( self ):
+        progress( "Stopping all modules" )
+        
+        self.r.L.go_slack()
+        self.r.R.go_slack()
+        self.r.Turret.go_slack()
+    """
+
+if __name__ == "__main__":
+    
+    app = diffDriveApp()
+    app.run()
+        
+        
