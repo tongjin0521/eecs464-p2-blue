9737b03a4d18ab5b1cc2c0bf3a9861e5c3a42712
Uriah <uriahjb@gmail.com>
2011-12-27 18:13:21 -0500

    Updated dynamixel2rpm method -U
    Former-commit-id: e38707859732e13e933510da4e216eb9495d4bc2

--- apps/diffDrive.py
+++ apps/diffDrive.py
@@ -15,6 +15,9 @@ class diffDriveApp( JoyApp ):
         kw.update( robot=dict(protocol=p, count=3) )
         JoyApp.__init__( self, confPath="$/cfg/diffDriveApp.yml", *args, **kw )
         self.r = self.robot.at
+        
+        self.drive = 0
+        self.turn = 0
         return        
     
     def stop( self ):
@@ -24,8 +27,24 @@ class diffDriveApp( JoyApp ):
         self.r.Turret.go_slack()
 
     def onStart( self ):
-        
+        fcp = FunctionCyclePlan( self, self.update, N=20, maxFreq=15, interval=0.05 )
+        fcp.onStart = curry( progress, "Initializing FCP" )
+        fcp.onStop = curry( progress, "Stopping FCP" )
+        self.fcp = fcp
 
+        sf = StickFilter( self )
+        sf.setLowpass( "joy0axis2", 5 )
+        sf.setLowpass( "joy0axis3", 5 )
+        
+        sf.start()
+        self.sf = sf
+        
+    def onStop( self ):
+        # If FunctionCyclePlan is running, don't forget to stop it
+        if self.fcp.isRunning():
+            self.fcp.stop()
+        # Also remember to stop all modules
+        self.stop()
     
 
 if __name__ == "__main__":
