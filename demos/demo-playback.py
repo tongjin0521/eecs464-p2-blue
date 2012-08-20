68f4d0130a3c8df533a00237343116b95893ca6a
Shai Revzen <shrevzen@eecs.umich.edu>
2012-08-19 23:36:28 -0400

    Adaptive timeout for large numbers of modules
    Former-commit-id: 9751db42f63e333291518f297322796cad6e2183

--- demos/demo-playback.py
+++ demos/demo-playback.py
@@ -4,7 +4,8 @@ from ckbot.posable import PoseRecorder, PoseRecorderCLI
 if __name__ == "__main__":
     import ckbot.logical as L
     c = L.Cluster()
-    c.populate(input("How many modules to .populate()? "))
+    n = input("How many modules to .populate()? ")
+    c.populate(n,timeout=n*0.5)
     pr = PoseRecorder(c.values())
     cli = PoseRecorderCLI(pr,c)
     cli.run()
