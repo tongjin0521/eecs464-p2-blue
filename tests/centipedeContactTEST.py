f258df7c5817ef1130437175fe1693b2cb1a40d0
Nick Quinnell <nicholas.quinnell@gmail.com>
2013-06-28 15:31:23 -0400

    now works for phi in the full range of [0,1]
    Former-commit-id: 185c1ba86fcfd5bdeef3be58d647de91223df734

--- tests/centipedeContactTEST.py
+++ tests/centipedeContactTEST.py
@@ -1,7 +1,7 @@
 '''
 	centipedeContactTEST
 	by Nick Quinnell
-	last updated 2013-06-11
+	last updated 2013-06-19
 	nicq@umich.edu
 	
 	test harness for centipedeContact module
@@ -15,16 +15,25 @@ from numpy import *
 
 	
 '''
-	put in code to plot different parts of gait in diff colors
-	prolly use find() to get the range of indexes for each part
-	of the gait, then plot the parts separately on the same graph
+	need to put in code to plot second half of gait
+	i.e. phi = [0.5, 1]
+	
+	also need to incorporate the code into the rest of the centipede code
+	create/update a progress message for the contact gait
+	then grep/tee/cut the output and put in a .csv file for easy
+	plotting and analysis
 '''	
 
-def printGait(roll, yaw, gaitState):
+def printGait(roll, yaw, gaitState, roll2, yaw2, gaitState2, phi):
+
+		
 	figure(1)
 	xlabel('Yaw')
 	ylabel('Roll')
+	
+	plotSecondHalf = True
 
+	#first half of gait
 	#plot 'front'
 	endFrontIdx = gaitState.index('stance') - 1
 	rollFront = roll[0:endFrontIdx]
@@ -44,44 +53,90 @@ def printGait(roll, yaw, gaitState):
 	plot(yawRecov, rollRecov, color='red')
 	
 	grid(True)
-	title('Yaw v Roll for contact gait')
+	title('Yaw v Roll for contact gait (side 1)\ncontact = cos(yaw) > 0.8')
 	xlim(-1.1, 1.1)
 	ylim(-1.1, 1.1)
-	show()
+	if not plotSecondHalf:
+		show()
+	
+	if plotSecondHalf:
+		#second half of gait
+		figure(2)
+		xlabel('Yaw')
+		ylabel('Roll')
+			
+		#get second half of gaitState list
+		endFrontIdx = gaitState2.index('stance') - 1
+		rollFront = roll2[0:endFrontIdx]
+		yawFront = yaw2[0:endFrontIdx]
+		plot(yawFront, rollFront, color='blue')
+		hold(True)
+	
+		#plot 'stance'
+		endStanceIdx = gaitState2.index('recovery') - 1
+		rollStance = roll2[endFrontIdx+1:endStanceIdx]
+		yawStance = yaw2[endFrontIdx+1:endStanceIdx]
+		plot(yawStance, rollStance, color='green')
+	
+		#plot 'recovery'
+		rollRecov = roll2[endStanceIdx+1:-1]
+		yawRecov = yaw2[endStanceIdx+1:-1]
+		plot(yawRecov, rollRecov, color='red')
+	
+		grid(True)
+		title('Yaw v Roll for contact gait (side 2)\ncontact = cos(yaw) > 0.8')
+		xlim(-1.1, 1.1)
+		ylim(-1.1, 1.1)
+		show()
+	
 
 	
 
 #set up parameters for the gait
 initParams = gaitParams()
-initParams.rollThresh = -0.7
-initParams.yawThresh = -0.8
+initParams.rollThresh = -0.99
+initParams.yawThresh = -0.9
 initParams.maxRoll = 1
 initParams.rollAmp = 1
 initParams.yawAmp = 1
 initParams.stanceVel = 10
+initParams.endRecovWindow = 0.2
 
 myGait = contactGait(initParams)
 #print('myGait.gaitState = ' + myGait.gaitState + '\n\n')
 
-numVals = 16
-phiVals = linspace(0, 0.5, numVals)
-rollVals = zeros(numVals, float)
-yawVals = zeros(numVals, float)
+#set up phi
+numVals = 2048
+phiVals = linspace(0, 1, numVals)
+
+#first half of gait
+rollVals = zeros(numVals/2, float)
+yawVals = zeros(numVals/2, float)
 gaitStates = list()
 
-textDebug = True
+#second half of gait
+rollVals2 = zeros(numVals/2, float)
+yawVals2 = zeros(numVals/2, float)
+gaitStates2 = list()
+
+textDebug = False
 
 i=0
 for phi in phiVals:
 	myGait.manageGait(phi)
-
-	rollVals[i] = myGait.roll
-	yawVals[i] = myGait.yaw
-	gaitStates.append(myGait.gaitState)
+	
+	if i < numVals/2:
+		rollVals[i] = myGait.roll
+		yawVals[i] = myGait.yaw
+		gaitStates.append(myGait.gaitState)
+	else:
+		rollVals2[i-numVals/2] = myGait.roll
+		yawVals2[i-numVals/2] = myGait.yaw
+		gaitStates2.append(myGait.gaitState)
+	
 	i += 1
 
 	if textDebug:
-		#should use matplotlib to visualize roll/yaw instead of printing
 		print('phi = ' + str(phi))
 		print('gaitState = ' + myGait.gaitState)
 		print('myGait.roll = ' + str(myGait.roll))
@@ -90,7 +145,8 @@ for phi in phiVals:
 		print('\n')
 
 
-printGait(rollVals, yawVals, gaitStates)
+printGait(rollVals, yawVals, gaitStates, rollVals2, yawVals2, 
+		  gaitStates2, phiVals)
 
 	
 
