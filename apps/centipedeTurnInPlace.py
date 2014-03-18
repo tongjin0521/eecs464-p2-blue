2efc82255aad5db2355668cdaf05c966ffaa362a
Nick Quinnell <nicholas.quinnell@gmail.com>
2014-03-18 16:42:32 -0400

    forgot to add the turn-in-place module
    Former-commit-id: 64bedd259729e482ec06847022f8c9e5048c0d27

---
+++ apps/centipedeTurnInPlace.py
@@ -0,0 +1,28 @@
+import math
+import centipedeContact as centCont
+
+class turnInPlaceGait(centCont.contactGait):
+    
+    def __init__(self, params):
+        centCont.contactGait.__init__(self, params, 1, 0)
+        
+        
+    def manageGait(self, phi):
+        #don't really need front/stance/recovery with Lissajous trajectory
+        #just do the calculation
+        self.__calcRollYaw(phi)
+        
+    def __calcRollYaw(self, phi):
+        '''Lissajous trajectory for roll/yaw
+            yaw = sin(2t)
+            roll = -sin(t)
+            
+            gives a figure-8
+        '''
+        pi = math.pi
+        self.yaw = math.sin(2*phi*2*pi) * self.params.yawAmp 
+        self.roll = -math.sin(phi*2*pi) * self.params.rollAmp
+        
+        return
+        
+        
