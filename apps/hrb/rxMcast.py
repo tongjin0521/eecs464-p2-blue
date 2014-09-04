9febe3f0e703633b3df8528a2d3f3152470cdd76
Shai Revzen (home VM) <shrevzen@umich.edu>
2014-09-04 19:49:13 -0400

    Interface specific to force IGMP out tunnel

--- apps/hrb/rxMcast.py
+++ apps/hrb/rxMcast.py
@@ -10,12 +10,14 @@ import struct
 
 MCAST_GRP = '224.0.1.133'
 MCAST_PORT = 2986
+IF_ADDR = "10.0.0.2"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('', MCAST_PORT))
-mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+sock.bind((IF_ADDR, MCAST_PORT))
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(IF_ADDR))
 
+mreq = socket.inet_aton(MCAST_GRP) + socket.inet_aton("0.0.0.0") #IF_ADDR)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 while True:
