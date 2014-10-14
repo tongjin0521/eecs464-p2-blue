711b17152f5319480f36cb8f6a9c39ddf61e1fa1
Shai Revzen (laptop VM) <shrevzen@umich.edu>
2014-10-13 20:11:36 -0400

    SCM controller seems to work
    Former-commit-id: 4e9d14f3e3902086b1329bb662c1a35329fe9be0

--- apps/scm-hex-mx.py
+++ apps/scm-hex-mx.py
@@ -1,6 +1,6 @@
 from joy.decl import *
 from joy import JoyApp, FunctionCyclePlan, progress, DEBUG
-from numpy import nan, asfarray, prod, isnan, pi, clip, sign
+from numpy import nan, asfarray, prod, isnan, pi, clip, sign, angle
 from cmath import exp
 from time import sleep, time as now
 from ckbot.dynamixel import MX64Module
@@ -18,7 +18,7 @@ SERVO_NAMES = {
 
 
 class ServoWrapperMX(object):
-    def __init__(self, servo, **kw):
+    def __init__(self, servo, logger=None, **kw):
         assert isinstance(servo, MX64Module), "only works with MX"
         self.servo = servo
         self.aScl = 36000.0  # servo units per rotation
@@ -26,9 +26,10 @@ class ServoWrapperMX(object):
         self.posOfs = 0
         self.ori = 1
         self.desAng = 0
-        self.desRPM = 0.1
-        self.Kp = 1
-        self.Kv = 0.03
+        self.desRPM = 00
+        self.Kp = 30
+        self.Kv = 0
+        self.logger = logger
         self._clearV()
         self._v = nan
         self.__dict__.update(kw)
@@ -37,19 +38,27 @@ class ServoWrapperMX(object):
 
     def _doCtrl(self):
         """execute an interaction of the controller update loop"""
-        a = exp(1j * self.get_ang())
-        a0 = exp(1j * self.desAng)
-        lead = a / a0
-        if lead.real < 0.7:
-            # outside of capture range; ignore
+        a = exp(1j * self.get_ang()*2*pi)
+        a0 = exp(1j * self.desAng*2*pi)
+        lead = angle(a / a0)
+        if abs(lead)>0.7*pi:
+            if "F" in DEBUG:
+                progress("FB desRPM %g out of range" % (self.desRPM))
+                    # outside of capture range; ignore
             return
-        pFB = clip(self.Kp * lead.imag, -0.5, 0.5)
+        pFB = clip(self.Kp * lead, -45, 45)
         if isnan(self._v):
             vFB = 0
         else:
             vFB = self.Kv * (self._v - self.desRPM)
         rpm = self.desRPM - pFB - vFB
-        progress("FB desRPM %g p %g v %g" % (self.desRPM, pFB, vFB))
+        if abs(rpm)<0.1:
+            rpm = 0
+        if "F" in DEBUG:
+            progress("FB desRPM %g p %g v %g" % (self.desRPM, pFB, vFB))
+        if self.logger:
+            self.logger.write( "ctrl", nid=self.servo.node_id,  
+                              desRPM=str(self.desRPM), pFB=str(pFB), vFB=str(vFB) )
         # Push into the motor
         self._set_rpm(rpm)
 
@@ -79,26 +88,22 @@ class ServoWrapperMX(object):
 
     def _set_pos(self, pos):
         self.servo.set_pos(pos)
-        sleep(2)
-
-    def set_abs_ang(self, ang):
-        self._set_servo()
-        pos = ang * self.ori * self.aScl
-        self._set_pos(pos)
-        self._set_motor()
-
 
     def set_ang(self, ang):
         self.desAng = ang
         self._doCtrl()
         if "s" in DEBUG:
             progress("%s.set_angle(%g)" % (self.servo.name, ang))
+        if self.logger:
+            self.logger.write( "set_ang", nid=self.servo.node_id, ang=str(ang) )
 
     def get_ang(self):
         pos = self.servo.get_pos()
         ang = (pos - self.posOfs) / self.aScl / self.ori
         if "g" in DEBUG:
             progress("%s.get_angle --> %g" % (self.servo.name, ang))
+        if self.logger:
+            self.logger.write( "get_ang", nid=self.servo.node_id, ang=str(ang) )
         self._updateV(ang)
         return ang
 
@@ -136,7 +141,7 @@ class ServoWrapperMX(object):
     def _set_rpm(self, rpm):
         """Push an RPM setting to the motor"""
         self._ensure_motor()
-        tq = clip(self.rpmScl * self.ori * rpm, -0.92, 0.92)
+        tq = clip(self.rpmScl * self.ori * rpm, -0.999, 0.999)
         if "r" in DEBUG:
             progress("%s.set_torque(%g) <-- rpm %g" % (self.servo.name, tq, rpm))
         self.servo.set_torque(tq + sign(tq) * 0.08)
@@ -147,56 +152,28 @@ class SCMHexApp(JoyApp):
         JoyApp.__init__(self, *arg, **kw)
 
     def onStart(self):
-        DEBUG.extend(list('vg'))
+        #DEBUG.extend(list('Fr'))
+        self.T0 = self.now
         self.leg = [
-            ServoWrapperMX(self.robot.at.FL, ori=-1),
-            ServoWrapperMX(self.robot.at.ML, ori=-1),
-            ServoWrapperMX(self.robot.at.HL, ori=-1),
-            ServoWrapperMX(self.robot.at.FR),
-            ServoWrapperMX(self.robot.at.MR),
-            ServoWrapperMX(self.robot.at.HR),
+            ServoWrapperMX(self.robot.at.FL, logger=self.logger, ori=-1),
+            ServoWrapperMX(self.robot.at.ML, logger=self.logger, ori=-1),
+            ServoWrapperMX(self.robot.at.HL, logger=self.logger, ori=-1),
+            ServoWrapperMX(self.robot.at.FR, logger=self.logger ),
+            ServoWrapperMX(self.robot.at.MR, logger=self.logger ),
+            ServoWrapperMX(self.robot.at.HR, logger=self.logger ),
         ]
         self.triL = self.leg[0::2]
         self.triR = self.leg[1::2]
-        self.fcp = FunctionCyclePlan(self, self._fcp_fun, 24, maxFreq=0.5, interval=0.05)
-        self.freq = 0.15
+        self.fcp = FunctionCyclePlan(self, self._fcp_fun, 32, maxFreq=0.75, interval=0.05)
+        #self.fcp = FunctionCyclePlan(self, lambda ignore : None, 256, maxFreq=0.5, interval=0.01)
+        self.freq = 45/60.0
         self.rate = 0.05
         self.limit = 1 / 0.45
         # set motors at initial position
-        for leg in self.triL:
-            leg.set_abs_ang(.25)
-        for leg in self.triR:
-            leg.set_abs_ang(-.25)
+        for leg in self.leg:
+            leg.set_ang(.25)
 
-        sleep(5)
-
-        for leg in self.triL + self.triR:
-            leg.set_rpm(32)
-
-    def _leg_to_phase(self, leg, phase):
-        """
-      Move a leg to a given phase, in position mode      
-      """
-        # if away from dead-zone phase
-        if abs(phase - 0.5) > 0.1:
-            # If leg might still be in dead zone --> poll it
-            if leg.isInDZ:
-                leg.get_pos()
-            # If outside dead zone --> use position control
-            if not leg.isInDZ:
-                leg.set_ang((phase - 0.5) * 36000)
-        else:  # else --> phase in dead zone range; switch to RPM control
-            # If not moving --> abort this
-            if self.fcp.period == 0:
-                return
-            rpm = 60 / self.fcp.period
-            leg.set_rpm(rpm)
-
-    def _fcp_fun(self, phase):
-        # Loop over all legs
-        v = []
-        vl = []
-        p = []
+    def _fcp_fun(self, phase): 
         # Desired angle for left and right tripods
         aL = phase - 0.5
         aR = ((phase + 0.5) % 1.0) - 0.5
@@ -204,8 +181,9 @@ class SCMHexApp(JoyApp):
         for leg, des in zip(self.triL + self.triR, aDes):
             leg.set_ang(des)
 
-
     def onEvent(self, evt):
+        if self.now-self.T0 > 10:
+            self.stop()
         if evt.type == KEYDOWN:
             if evt.key in [ord('q'), 27]:  # 'q' and [esc] stop program
                 self.stop()
@@ -234,6 +212,7 @@ class SCMHexApp(JoyApp):
                 #
             else:  # any other key
                 progress("Starting cycles....")
+                self.fcp.setPeriod(1 / self.freq)
                 self.fcp.start()
             return  # (from all keypress handlers)
         if evt.type not in [TIMEREVENT, JOYAXISMOTION, MOUSEMOTION]:
@@ -268,6 +247,7 @@ if __name__ == '__main__':
         # for actual operation use w/ arch=DX & NO required line:
         L.DEFAULT_BUS = DX
         app = SCMHexApp(
+            cfg = dict( logFile = "/tmp/log" ),
             robot=dict(arch=DX, count=len(SERVO_NAMES), names=SERVO_NAMES)
         )
     else:
@@ -281,3 +261,13 @@ if __name__ == '__main__':
     joy.DEBUG[:] = []
     app.run()
 
+
+'''
+# debug code plotting
+import joy
+l = asfarray( [ (e['TIME'], e['ang'], e['nid']) for e in joy.loggit.iterlog('/tmp/log.gz') if e['TOPIC'] == "set_ang" ] )
+g = asfarray( [ (e['TIME'], e['ang'], e['nid']) for e in joy.loggit.iterlog('/tmp/log.gz') if e['TOPIC'] == "get_ang" ] )
+for k in set(unique(l[:,2])):
+    plot( g[g[:,2]==k,0], g[g[:,2]==k,1], '+' )
+    plot( l[l[:,2]==k,0], l[l[:,2]==k,1], '.' )
+'''
