227766422feb7ff0b5b7836492110ace499de97e
Lab shared laptop <shrevzen@umich.edu>
2015-04-18 11:57:32 -0400

    misc changes

--- py/joy/__init__.py
+++ py/joy/__init__.py
@@ -325,7 +325,7 @@ class JoyApp( object ):
       keyboardRepeatInterval = 50,
       clockInterval = 20,
       voltagePollRate = 1.0,
-      minimalVoltage = 10,
+      minimalVoltage = 13,
       windowSize = [320,240],
       nodeNames = {},
       logFile = None,
