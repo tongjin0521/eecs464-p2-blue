c08d29869902cc4501564e5904f79a183a37e343
shrevzen (2017 VM) <shrevzen@umich.edu>
2017-02-09 16:28:24 -0500

    Support for AnimatorPlan

--- apps/hrb/tagClient.py
+++ apps/hrb/tagClient.py
@@ -1,6 +1,9 @@
-from pylab import figure, gcf, plot, axis, text, find, draw, clf
+from joy import JoyApp
+from joy.plans import AnimatorPlan
+from joy.decl import KEYDOWN  
+
 from socket import socket, AF_INET, SOCK_DGRAM, error as SocketError
-from numpy import asarray, array, mean
+from numpy import array, mean
 from json import loads as json_loads
 
 if __name__ != "__main__":
@@ -11,32 +14,46 @@ try:
 except Exception:
   pass
 
-s = socket(AF_INET, SOCK_DGRAM )
-s.bind(("",0xB00))
-s.setblocking(0)
+def _animation(fig):
+  s = socket(AF_INET, SOCK_DGRAM )
+  s.bind(("",0xB00))
+  s.setblocking(0)
+  fig.clf()
+  ax = fig.add_subplot(111)
+  msg = None
+  while True:
+    try:
+      # read data as fast as possible
+      m = s.recv(1<<16)
+      if m and len(m)>2:
+        msg = m
+      continue
+    except SocketError, se:
+      # until we've run out; last message remains in m
+      pass
+    # make sure we got something
+    if not msg:
+      yield
+      continue
+    # display it
+    ax.cla()
+    for d in json_loads(msg):
+      a = array(d['p'])
+      ax.plot( a[[0,1,2,3,0],0], a[[0,1,2,3,0],1], '.-b' )
+      ax.plot( a[[0],0], a[[0],1], 'og' )
+      ax.text( mean(a[:,0]), mean(a[:,1]), d['i'], ha='center',va='center' )
+      ax.axis([0,1024,0,768])
+    yield
+    
+      
+class App(JoyApp):
+  def onStart(self):
+      AnimatorPlan(self,_animation).start()
 
-lst = []
-msg = None
-while True:
-  try:
-    # read data as fast as possible
-    m = s.recv(1<<16)
-    if m and len(m)>2:
-      msg = m
-    continue
-  except SocketError, se:
-    # until we've run out; last message remains in m
-    pass
-  # make sure we got something
-  if not msg:
-    continue
-  # display it
-  clf()
-  for d in json_loads(msg):
-    a = array(d['p'])
-    plot( a[[0,1,2,3,0],0], a[[0,1,2,3,0],1], '.-b' )
-    plot( a[[0],0], a[[0],1], 'og' )
-    text( mean(a[:,0]), mean(a[:,1]), d['i'], ha='center',va='center' )
-    axis([0,1024,0,768])
-  draw()
+  def onEvent(self,evt):
+      if evt.type == KEYDOWN:
+        return JoyApp.onEvent(self,evt)
+        
+app = App(cfg={'windowSize':(1080,740)})
+app.run()
   
