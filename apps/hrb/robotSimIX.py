2ab21a8f029fc23b7bc3d1174b3317e33cd2efdb
Shai Revzen (Laptop X1) <shrevzen@umich.edu>
2018-01-29 23:55:26 -0500

    Rescaled simulated arena to realistic size
    Former-commit-id: 5a58a93553902335d0c2f7514b4dd352362a5b2e

--- apps/hrb/robotSimIX.py
+++ apps/hrb/robotSimIX.py
@@ -15,36 +15,36 @@ from pdb import set_trace as DEBUG
 
 
 MSG_TEMPLATE = {
-       0: [[502, 251], [479, 272], [508, 296], [530, 274]],
-       1: [[469, 347], [471, 311], [434, 305], [431, 339]],
-       2: [[466, 226], [437, 209], [420, 231], [450, 248]],
-       3: [[362, 204], [339, 224], [365, 245], [387, 224]],
-       4: [[334, 336], [370, 326], [359, 294], [324, 301]],
-       22: [[425, 145], [424, 169], [455, 173], [455, 149]],
-       23: [[556, 302], [594, 308], [588, 277], [553, 272]],
-       24: [[284, 258], [294, 229], [261, 221], [250, 249]],
-       25: [[432, 424], [431, 384], [391, 380], [392, 419]],
-       26: [[559, 399], [564, 440], [606, 444], [599, 403]],
-       27: [[546, 163], [546, 187], [578, 193], [576, 170]],
-       28: [[212, 397], [253, 395], [254, 355], [214, 358]],
-       29: [[319, 129], [290, 124], [283, 145], [313, 153]]
+    0 : [[2016, 1070], [1993, 1091], [2022, 1115], [2044, 1093]],
+    1 : [[1822, 1323], [1824, 1287], [1787, 1281], [1784, 1315]],
+    2 : [[1795, 911], [1766, 894], [1749, 916], [1779, 933]],
+    3 : [[1451, 876], [1428, 896], [1454, 917], [1476, 896]],
+    4 : [[1374, 1278], [1410, 1268], [1399, 1236], [1364, 1243]],
+    22 : [[1744, 622], [1743, 646], [1774, 650], [1774, 626]],
+    23 : [[2274, 1171], [2312, 1177], [2306, 1146], [2271, 1141]],
+    24 : [[1100, 975], [1110, 946], [1077, 938], [1066, 966]],
+    25 : [[1666, 1629], [1665, 1589], [1625, 1585], [1626, 1624]],
+    26 : [[2305, 1663], [2310, 1704], [2352, 1708], [2345, 1667]],
+    27 : [[2230, 697], [2230, 721], [2262, 727], [2260, 704]],
+    28 : [[911, 1525], [952, 1523], [953, 1483], [913, 1486]],
+    29 : [[1222, 542], [1193, 537], [1186, 558], [1216, 566]],
   }
-  
+
 def tags2list( dic ):
-    """ 
+    """
     Convert a dictionary of tags into part of a list for JSON serialization
-    
+
     INPUT:
       dic -- dictionary mapping tag id to 4x2 corner location array
-      
+
     OUTPUT:
       list to be concatenated into JSON message
     """
-    return [ 
-        { 
-          u'i' : k, 
-          u'p': [ list(row) for row in v ] 
-        } 
+    return [
+        {
+          u'i' : k,
+          u'p': [ list(row) for row in v ]
+        }
         for k,v in dic.iteritems()
     ]
 
@@ -52,11 +52,11 @@ def findXing(a,b):
   """
   Find the crossing point of two lines, represented each by a pair of
   points on the line
-  
+
   INPUT:
     a -- 2x2 -- two points, in rows
     b -- 2x2 -- two points, in rows
-  
+
   OUTPUT: c -- 2 -- a point, as an array
   """
   a = asfarray(a)
@@ -73,24 +73,24 @@ def findXing(a,b):
   q = Q[:2,2]/Q[2,2]
   c = q[0]*(a[1]-a[0])+a[0]
   return c
-  
+
 class RobotSimInterface( object ):
   """
   Abstract superclass RobotSimInterface defines the output-facing interface
   of a robot simulation.
-  
+
   Subclasses of this class must implement all of the methods
   """
   def __init__(self, fn=None):
     """
     INPUT:
-      fn -- filename / None -- laser log name to use for logging simulated 
+      fn -- filename / None -- laser log name to use for logging simulated
           laser data. None logged if name is None
-          
+
     ATTRIBUTES:
       tagPos -- 4x2 float array -- corners of robot tag
       laserAxis -- 2x2 float array -- two points along axis of laser
-      waypoints -- dict -- maps waypoint tag numbers to 4x2 float 
+      waypoints -- dict -- maps waypoint tag numbers to 4x2 float
           arrays of the tag corners
     """
     # Initialize dummy values into robot and arena state
@@ -107,11 +107,11 @@ class RobotSimInterface( object ):
       self.out = None
     else:
       self.out = opengz(fn,"w")
-    
+
   def refreshState( self ):
     """<<pure>> refresh the value of self.tagPos and self.laserAxis"""
     print "<<< MUST IMPLEMENT THIS METHOD >>>"
-    
+
   def getTagMsg( self ):
     """
     Using the current state, generate a TagStreamer message simulating
@@ -133,21 +133,21 @@ class RobotSimInterface( object ):
     Using the current state, generate a fictitious laser pointer reading
     INPUT:
       now -- float -- timestamp to include in the message
-    
-    OUTPUT: string of human readable message (not what is in log)      
+
+    OUTPUT: string of human readable message (not what is in log)
     """
     x = findXing( self.laserScreen, self.laserAxis )
     if self.out:
-      self.out.write("%.2f, 1, %d, %d\n" % (now,n+1,x[0],x[1]))          
+      self.out.write("%.2f, 1, %d, %d\n" % (now,n+1,x[0],x[1]))
     return "Laser: %d,%d " % tuple(x)
-    
-    
+
+
 class DummyRobotSim( RobotSimInterface ):
   def __init__(self, *args, **kw):
     RobotSimInterface.__init__(self, *args, **kw)
     self.dNoise = 0.1
     self.aNoise = 0.1
-    
+
   def move( self, dist ):
     """
     Move forward some distance
@@ -166,16 +166,15 @@ class DummyRobotSim( RobotSimInterface ):
     zr = c + (z-c) * exp(1j * (angle+randn()*self.aNoise))
     self.tagPos[:,0] = zr.real
     self.tagPos[:,1] = zr.imag
-    
+
   def refreshState( self ):
     """
     Make state ready for use by client.
-    
+
     ALGORITHM:
     Since the entire robot state is captured by the location of the
-    robot tag corners, update the laser axis from the robot tag location 
+    robot tag corners, update the laser axis from the robot tag location
     """
     self.laserAxis = dot([[1,1,0,0],[0,0,1,1]],self.tagPos)/2
     da = dot([1,-1],self.laserAxis)
     self.laserAxis[1] += randn(2) * sqrt(sum(da*da)) * 0.01
-    
