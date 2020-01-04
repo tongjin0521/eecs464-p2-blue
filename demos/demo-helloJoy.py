400ddda2aee962959b047cca2ceed056bb816c6e
Shai Revzen (Laptop X1) <shrevzen@umich.edu>
2020-01-04 17:54:15 -0500

    Docs

--- demos/demo-helloJoy.py
+++ demos/demo-helloJoy.py
@@ -8,25 +8,25 @@ from joy import JoyApp
 
 class HelloJoyApp( JoyApp ):
   """HelloJoyApp
-  
+
      The "hello world" of JoyApp programming.
      This JoyApp pipes the y coordinate of mouse positions (while left
      button is pressed) to a specified setter. By default this setter is
-     given by "#output " -- i.e. it is a debug message. 
-     
+     given by "#output " -- i.e. it is a debug message.
+
      See JoyApp.setterOf() for a specification of possible outputs
 
      This file is intended to introduce you to our joy library
   """
   def __init__(self,spec,*arg,**kw):
     # This is a "constructor". It initializes the JoyApp object.
-    # Because we added an additional parameter, we extend the 
+    # Because we added an additional parameter, we extend the
     # JoyApp constructor. The first step is to call the superclass
     # constructor so that we'll have a valid JoyApp instance.
     JoyApp.__init__(self, *arg,**kw)
     # Store output specifier for later use
     self.spec = spec
-    
+
   def onStart(self):
     # This function is called when the JoyApp is ready to start up,
     # i.e. after all PyGame devices have been activated, robot Cluster
@@ -40,6 +40,13 @@ class HelloJoyApp( JoyApp ):
     # If we reach this line, it was a MOUSEMOTION with button pressed
     #   so we send the value out to the output
     self.output( evt.pos[1] )
-if __name__=="__main__":  
+if __name__=="__main__":
+  print("""
+  Uses MOUSEMOTION events; you may need to restart your 'ctrl' process with:
+  ctrl -e MOUSEMOTION
+
+  This demo will output mouse drag x coordinates to the designated output CLP.
+  If mouse button isn't pressed, the event is punted to superclass.
+  """)
   app = HelloJoyApp("#output ")
   app.run()
