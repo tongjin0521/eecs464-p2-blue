6207f7b80633a60426f81875926146f2bca2268b
Shai Revzen (Laptop X1) <shrevzen@umich.edu>
2020-03-31 04:20:50 -0400

    Simulated arm, with page plotting enabled

--- hrb/p2sim.py
+++ hrb/p2sim.py
@@ -5,14 +5,40 @@ Created on Mon Mar 23 01:14:25 2020
 
 @author: shrevzen
 """
+from time import clock
 from numpy import ( 
-    asarray, stack, ones, identity, dot, newaxis, cumsum, c_
+    asarray, stack, ones, identity, dot, newaxis, cumsum, c_, nan, inf
 )
 from numpy.linalg import inv
 from arm import Arm, jacobian_cdas
 from motorsim import MotorModel,TH,BL
-from pylab import gca
 from joy.plans import AnimatorPlan
+from joy import JoyApp
+from joy.decl import KEYDOWN, K_q, K_ESCAPE, progress
+from vis3d import FourViewPlot, xyzCube, iCube, iFace, plotVE
+from joy.misc import requiresPyGame
+requiresPyGame()
+
+# Student team selection -- transform from workspace coordinates to world
+Tws2w = asarray([
+     [1,0,0,  0],
+     [0,1,0, -5],
+     [0,0,1,-10],
+     [0,0,0,  1]
+]) 
+# Arm specification
+armSpec = asarray([
+        [0,0.02,1,  4,  0],
+        [0,   1,0,  4,  0],
+        [0,   1,0,  4,  0],
+    ]).T
+# Transform of paper coordinates to workspace
+Tp2ws = asarray([
+     [0.7071,0,-0.7071,0],
+     [0,     1,      0,0],
+     [0.7071,0, 0.7071,0],
+     [0,     0,      0,1]
+])
 
 class MassArm(Arm):
     def __init__(self):
@@ -100,9 +126,8 @@ class MassArm(Arm):
       fI = [ dot(dot(ia,ic),ia.T) for ia,ic in zip(iA,Ic) ]
       return fI
     
-    def plot3D(self,A,ax=None):
+    def plot3D(self,A,ax):
       Arm.plot3D(self,A,ax)
-      ax = gca()
       cx,cy,cz,_ = self.getCoMs(A).T
       ax.plot3D(cx,cy,cz,'ow')
       ax.plot3D(cx,cy,cz,'+k')
@@ -111,14 +136,24 @@ class ArmSim(MassArm):
     def __init__(self,wlc):
         MassArm.__init__(self)
         wlc = asarray(wlc)
-        if wlc.shape[0] < 5:
-          wlc = c_[wlc.T,ones((wlc.shape[1],1))].T
+        assert wlc.ndim == 2 and wlc.shape[0] == 5
         self.setup(wlc[:-1])
-        self.c = wlc[-1]
         self.m = []
         for k in range(wlc.shape[1]):
-          self.m.append(MotorModel())
-
+          mm = MotorModel()
+          # Set initial angle
+          mm.clear([wlc[4,k]])
+          # Set initial goal angle to match
+          mm.set_pos(mm.get_pos())
+          # Store in arm config
+          self.m.append(mm)
+        # Set joint compliance
+        self.c = ones(len(self.m))
+        
+    def __iter__(self):
+        for mm in self.m:
+          yield mm
+          
     def __getitem__(self,idx):
         return self.m[idx % len(self.m)]
     
@@ -153,92 +188,176 @@ class ArmSim(MassArm):
             mi._ext = -tqi
         ang1 = ang0 - self.c*tq # sagged angles
         return t,ang1,stack(y,1)
-    
-from joy import JoyApp
-from joy.decl import KEYDOWN,K_q,K_ESCAPE,progress
-from joy.misc import requiresPyGame
-requiresPyGame()
-
+      
 class ArmAnimatorApp( JoyApp ):
     def __init__(self,wlc,*arg,**kw):
+      if 'cfg' not in kw:
+        kw['cfg'] = {}
+      kw['cfg'].update(windowSize = [1200, 800])
       JoyApp.__init__(self,*arg,**kw)
       self.arm = ArmSim(wlc)
+      self.Tp2w = dot(Tws2w,Tp2ws)
+      # World to paper
+      self.Tw2p = inv(self.Tp2w)
+      # Paper with origin at origin
+      self.paper_p = asarray([[8,11,-1/2.56,1]])*xyzCube
+      self.paper_w = dot(self.paper_p,self.Tp2w.T)  
+      # Workspace box
+      L = 12 # Workspace in arm units
+      self.ws_w = dot(asarray([[L,L,L,1]])*xyzCube,Tws2w.T)
+      self.ws_p = dot(self.ws_w,self.Tw2p.T)
+      # Projection onto paper in world
+      self.Tprj = dot(self.Tp2w,asarray([[1,1,0,1]]).T*self.Tw2p)
+      # World to relative paper (i.e. paper is unit cube)
+      self.Tw2rp = self.Tw2p / self.paper_p[-1][:,newaxis]
       
     def onStart(self):
       self.ani = AnimatorPlan(self,self._animation)
-      self.t,self.q,self.y,self.p = [],[],[],[]
+      self.t,self.q,self.y,self.p,self.l = [],[],[],[],[]
       self.T0 = self.now
       self.ani.start()
-    
-    def _animation(self, fig):
+
+    def _integrate(self):
       last = self.T0
-      ax = fig.add_subplot(111,projection='3d')
+      dt = 0.1
+      qi = None
+      ti = None
       while True:
-        # If enough time elapsed to animate
+        # Run in units of dt
         now = self.now
         h = now - last
-        if h<0.1:
+        if h<dt and ti:
           yield
           continue
+        last = now
         ti,qi,yi =self.arm.step(h) # Speedup ratio into simulation time
         # Store simulation time-step
         self.t.append(ti)
         self.q.append(qi)
         self.y.append(yi)
-        self.p.append(self.arm.getTool(qi))
-        # Display it
-        ax.cla()
-        self.arm.plot3D(self.arm.at(qi),ax)
-        p = asarray(self.p[-20:]).T
-        ax.plot3D(p[0],p[1],p[2],'.-k',alpha=0.3)
-        ax.set_title("t = %.2f" % ti)
-        last = now
-        progress("TEMP: "+" ".join(["%d:%-6.2f" % (n,self.arm[n].get_temp()) for n,qqi in enumerate(qi)]))
+        pen = self.arm.getTool(qi)
+        self.p.append(pen)
+        # Line marking point is lowpass version of pen
+        if self.l:
+          self.l.append(self.l[-1] * 0.6 + pen * 0.4)
+        else:
+          self.l.append(pen)
+
+    def _show(self,fvp):
+        ti = self.t[-1]
+        qi = self.q[-1]
+        pen = self.p[-1]
+        li = self.l[-1]
+        # Display it, world frame image
+        fvp.cla()
+        fvp.set(xticks=[],yticks=[])
+        # self.arm.plot3D(self.arm.at(qi),fvp)
+        sk = self.arm.getSkel(self.arm.at(qi)).T
+        fvp.plot3D(sk[0],sk[1],sk[2],lw=3,marker='o',color='#808080')
+        # Find current pen point in paper coordinates
+        qq = dot(self.Tw2rp,pen)
+        # If in sheet
+        if (qq[0]>0) and (qq[0]<1) and (qq[1]>0) and (qq[1]<1):
+          if qq[2]>1: # Pressed too far in
+            lt = dict(marker='o',color='r')
+          elif qq[2]>0: # Drawing
+            lt = dict(marker='.',color='g')
+          else: # Above sheet
+            lt = dict(marker="+", ms=15, color="m")
+        else: # Not above or below sheet
+            lt = dict(marker="+",color="b")
+        # Project onto paper and bring back to world coordinates
+        wp = dot(self.Tprj,pen)
+        fvp.plot3D([wp[0]],[wp[1]],[wp[2]],**lt)
+        wl = dot(self.Tprj,li)
+        fvp.plot3D([wl[0]],[wl[1]],[wl[2]],'xk')
+        fvp.xyz.set_title("t = %.2f, n=%d" % (ti,len(self.t)))
+        plotVE(fvp,self.paper_w,iCube,'g--',alpha=0.3)
+        plotVE(fvp,self.paper_w[::2,:],iFace,'g-')
+        plotVE(fvp,self.ws_w,iCube,'k:')
+      
+    def _animation(self, fig):
+      fig.clf()
+      last = self.T0
+      fvp = FourViewPlot(fig,f=inf)
+      sim = self._integrate()
+      dt = 0.1
+      while True:
+        next(sim) # allow simulation to run        
+        # Make sure graphics run at most 33% of the time
+        now = self.now
+        if now-last < dt * 2:
+          yield
+          continue
+        last = now        
+        # Draw graphics, measure execution time
+        tic = clock()
+        self._show(fvp)
+        dt = clock() - tic
+        # Display progress
+        progress("(%4.2f) " % ( clock()-tic) +
+            " ".join(["%15s" % (
+            "%d/%d/%d" % (m.get_pos(),m.get_goal(),int(m.get_temp())) 
+            ) for m in self.arm
+        ]),sameLine=True)
         yield
 
     def onEvent(self,evt):
-      # Punt everything except keydown events
-      if evt.type != KEYDOWN or evt.key in {K_q, K_ESCAPE}:
+      # Ignore everything except keydown events
+      if evt.type != KEYDOWN:
+        return
+      # Punt exit keys
+      if evt.key in {K_q, K_ESCAPE}:
         return JoyApp.onEvent(self,evt)
       # row of 'a' on QWERTY keyboard increments motors
       p = "asdfghjkl".find(evt.unicode)
       if p>=0:
-        self.arm[p].set_pos(self.arm[p].get_pos() + 500)
+        self.arm[p].set_pos(self.arm[p].get_goal() + 500)
         return
       # row of 'z' in QWERTY keyboard decrements motors
       p = "zxcvbnm,.".find(evt.unicode)
       if p>=0:
-        self.arm[p].set_pos(self.arm[p].get_pos() - 500)
-        return
-      
+        self.arm[p].set_pos(self.arm[p].get_goal() - 500)
+        return      
       
     def onStop(self):
+      # We need this kind of import so as not to mess up JoyApp plotting
       from pylab import figure,savefig
+      # Collect pen motions and tool tip motions
       p = asarray(self.p).T
-      fig = figure(1)
-      ax3d = fig.add_subplot(221,projection='3d')
-      ax3d.plot3D(p[0],p[1],p[2],'.-k')
-      ax = fig.add_subplot(222)
-      ax.plot(p[0],p[1],'.-k')
-      ax.set(xlabel="x",ylabel="y")
-      ax.axis('equal')
-      ax.grid(1)
-      ax = fig.add_subplot(224)
-      ax.plot(p[0],p[2],'.-k')
-      ax.set(xlabel="x",ylabel="z")
-      ax.axis('equal')
-      ax.grid(1)
-      ax = fig.add_subplot(223)
-      ax.plot(p[1],p[2],'.-k')
-      ax.set(xlabel="y",ylabel="z")
-      ax.axis('equal')
-      ax.grid(1)
-      savefig("result.png")
+      l = asarray(self.l).T
       t = asarray(asarray(self.t)*100,int)
-      pout = c_[t,asarray(p[:3]*100,int).T]
+      # Convert to paper coordinates
+      qq = dot(self.Tw2rp,p)
+      lp = dot(self.Tw2p,l)
+      # Prepare output as integers:
+      #  time, x, y, depth
+      pout = c_[t,asarray(lp[:2]*100,int).T,asarray(qq[2]*100,int)]
       with open("result.csv","w") as rf:
         for pp in pout:
           rf.write(repr(list(pp))[1:-1]+"\n")
+      # Draw on "paper"
+      fig = figure(2)
+      fig.clf()
+      ax = fig.gca()
+      # projected track is pale blue
+      ax.plot(lp[0],lp[1],'-b',alpha=0.2)
+      # Find when pen was up, and remove those points
+      up = qq[2]<0
+      lp[:,up]=nan
+      # Draw in black
+      ax.plot(lp[0],lp[1],'-k',lw=2)
+      # Find when pen was jammed in paper
+      bad = qq[2]>1
+      if any(bad): # If found, highlight in red
+        lp[:,~bad] = nan
+        ax.plot(lp[0],lp[1],'.-r',lw=3,alpha=0.5)
+      # Draw the frame of the paper
+      fr = self.paper_p[::2,[0,1]][[0,1,3,2,0]].T
+      ax.plot(fr[0],fr[1],'b--',lw=2)
+      ax.axis('equal')
+      ax.grid(1)
+      savefig("result.png",dpi=300)
       
       
 if __name__=="__main__":
@@ -251,10 +370,6 @@ if __name__=="__main__":
     'q' will quit and store the results in a results.png image and results.csv
     file.
     """
-    app = ArmAnimatorApp(asarray([
-        [0,0.02,1,4],
-        [0,1,0,4],
-        [0,1,0,4],
-    ]).T)
+    app = ArmAnimatorApp(armSpec)
     app.run()
     
