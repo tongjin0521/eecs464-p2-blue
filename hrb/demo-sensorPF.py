4b28dc8bafb5bbce6dbda964297387a12f58bc78
Shai Revzen (Laptop X1) <shrevzen@umich.edu>
2022-02-06 16:57:46 -0500

    Updated animation and added periodic excitation

--- hrb/demo-sensorPF.py
+++ hrb/demo-sensorPF.py
@@ -4,9 +4,11 @@ FILE: demo-sensorPfig.py
   Demonstration of how to use a particle filter to process line sensor 
   responses
 """
+from time import time
 from joy import JoyApp
 from joy.plans import AnimatorPlan
-from pylab import rand,ones,asarray,randn,find,prctile,linspace,arange,argmax
+from numpy import ones,asarray,linspace,arange,argmax,percentile
+from numpy.random import rand,randn
 from waypointShared import lineSensorResponse, lineDist
 
 def _animation(fig):
@@ -14,45 +16,44 @@ def _animation(fig):
   Num = 50
   # Step size
   stepSize = 0.02
-  # Sensor Noise 
+  # Sensor Noise
   sNoi = 0.001
   # Movement Noise
   mNoi = 0.01
   # Min percentile for keeping particle
-  minP = 25        
+  minP = 25
   # Locations
   pts = rand(Num)+1j*rand(Num)
   # Weights
   wgt = ones(Num)
   # Line
-  abLine = asarray([.2+0.5j,.8+0.7j])
+  abLine = asarray([-.5-0.4j,.8+0.3j])
   # Actual position
   pos = 0.5+0.5j
   while True:
     ### "robot" simulation
     # Move "robot"
-    step = (randn() + randn()*1j) * stepSize
+    step = (randn() + randn()*1j) * stepSize-pos*0.05 +sin(time()/2)*0.06
     pos += step
     # Get "robot" sensor measurement
     dpos = lineDist([pos],abLine[0],abLine[1])
-    spos = lineSensorResponse(dpos, sNoi)        
+    spos = lineSensorResponse(dpos, sNoi)
     ### Particle filter
     # Move particles
     pts += step + (randn(Num)+1j*randn(Num)) * mNoi
     # Get particle sensor measurements
     d = lineDist(pts,abLine[0],abLine[1])
-    s = lineSensorResponse(d, sNoi)        
+    s = lineSensorResponse(d, sNoi)
     # Adjust weights, keeping matching sensors with heigher weight
     #   We penalize sensor mismatch
     #   We penalize all high weights. Because average weight is reset to 1.0,
     #     this implies that in absence of 
-    #wgt = 1.0+abs(spos-s)*0.5+wgt*0.1
     wgt = wgt * 0.6 + 0.4 / (1.0+abs(spos-s)*0.5)
-    wgt = Num * wgt / sum(wgt)        
+    wgt = Num * wgt / sum(wgt)
     # Find best particle
-    best = argmax(wgt)      
+    best = argmax(wgt)
     # Replace low weight particles
-    idx = find(wgt<prctile(wgt,minP))
+    idx = (wgt<percentile(wgt,minP)).nonzero()[0]
     if any(idx):
       pts[idx] = pts[best]
       wgt[idx] = 1.0
@@ -63,7 +64,7 @@ def _animation(fig):
     a.scatter(pts.real, pts.imag, s=10+wgt*wgt*4, c=linspace(0,1,Num), alpha=0.5 )
     a.plot( pos.real, pos.imag, 'r+', ms = 15, mew=3)
     a.plot( pts[best].real, pts[best].imag, 'bx', ms = 15, mew=2)
-    a.axis('equal'); a.axis([-0.5,1.5,-0.5,1.5])
+    a.axis('equal'); a.axis([-1,1,-1,1])
     a = fig.add_subplot(122)
     a.bar(arange(Num),wgt)
     yield
@@ -71,8 +72,8 @@ def _animation(fig):
 class App(JoyApp):
   def onStart(self):
     AnimatorPlan(self,_animation).start()
-        
+
 if __name__=="__main__":
   app = App()
   app.run()
-  
+
