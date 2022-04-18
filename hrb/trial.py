00d0e788884c4bd16f5bd36556a251938e0221c7
tongjin <tongjin@umich.edu>
2022-04-17 21:01:14 -0400

    new func

--- hrb/trial.py
+++ hrb/trial.py
@@ -7,8 +7,18 @@ def discretize_square(x, y, s, left_upper_p, right_lower_p):
     pass
 
 def cal_corner_pts(recorded_pts):
-    left_x = 
-    pass
+    recorded_pts = np.array(recorded_pts).reshape(8,3)
+    left_upper_p = np.array([[-1.],[-1.],[-1.]])
+    right_lower_p =  np.array([[-1.],[-1.],[-1.]])
+    left_upper_p[0,0] = np.mean(recorded_pts[:3,0])
+    left_upper_p[1,0] = (np.mean(recorded_pts[-2,1]) * 2 + recorded_pts[0,1])/3
+    left_upper_p[2,0] = np.mean(recorded_pts[:3,2])
+    right_lower_p[0,0] = np.mean(recorded_pts[4:7,0])
+    right_lower_p[1,0] = np.mean(recorded_pts[2:5,1])
+    right_lower_p[2,0] = np.mean(recorded_pts[4:7,2])
+    print(left_upper_p)
+    print(right_lower_p)
+    return left_upper_p,right_lower_p
 
 input =[np.array([[56.837089], [4.05496933],[34.52064423]] ),
 np.array([[56.7680284], [-5.92928143] ,[34.88270637]] ),
@@ -41,5 +51,5 @@ np.array([[48.82450653], [4.38530711], [24.27425379]])]
 
 left_upper_p, right_lower_p = cal_corner_pts(input)
 
-target_square = discretize_square(0 , 0, 10.5,tp2w)
-print(target_square)
+# target_square = discretize_square(0 , 0, 10.5,left_upper_p,right_lower_p)
+# print(target_square)
